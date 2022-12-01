0ec5fbfe6eaaee0afeadeeaa9a7109200a7af5a9
EgorB <96645276+Egor1515@users.noreply.github.com>
2022-12-01 23:25:46 +1000

    5123

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,12 +1,7 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
-
-import java.sql.Time;
-import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,14 +12,5 @@ public class MainTest {
     private final ElementsCollection foundVideos =$$("img.yt-core-image--fill-parent-height.yt-core-image--fill-parent-width.yt-core-image.yt-core-image--content-mode-scale-aspect-fill.yt-core-image--loaded");
 //    private SelenideElement = $("");
 //    private SelenideElement = $("");
-    @Test
-    public void should(){
-        Configuration.holdBrowserOpen = true;
 
-        open("https://youtube.com");
-
-
-
-
-    }
 }
